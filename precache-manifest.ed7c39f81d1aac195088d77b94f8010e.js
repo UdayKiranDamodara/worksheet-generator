ca256959ed0ea643ac978300285bcedd3afb2d59
UdayKiraDamodara <udaykirandamodara@gmail.com>
2021-01-06 13:44:19 +0530

    Updates

--- precache-manifest.ed7c39f81d1aac195088d77b94f8010e.js
+++ precache-manifest.ed7c39f81d1aac195088d77b94f8010e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b251c1da3c51b6011925b6bf056adc46",
+    "revision": "17b3b125b60aec3faf815551cfe6f622",
     "url": "/worksheet-generator/index.html"
   },
   {
-    "revision": "21b667a04802ca16fece",
+    "revision": "01c6197374b68fb0f721",
     "url": "/worksheet-generator/static/css/main.556c60f4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/worksheet-generator/static/js/2.12c8d164.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "21b667a04802ca16fece",
-    "url": "/worksheet-generator/static/js/main.c570d357.chunk.js"
+    "revision": "01c6197374b68fb0f721",
+    "url": "/worksheet-generator/static/js/main.bd1d04a3.chunk.js"
   },
   {
     "revision": "4608f632b66b0358fd13",
